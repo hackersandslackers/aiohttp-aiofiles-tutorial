1275d117b34a8c7729dad9e55b0bced710f17b35
Todd Birchard <toddbirchard@gmail.com>
2021-09-27 02:53:56 -0400

    test_parser.py

--- data/tests/test_parser.py
+++ data/tests/test_parser.py
@@ -1,9 +1,11 @@
 import pytest
 from typing import List
 
-from data import urls
+from data import parse_urls
 
 
 def test_parse_urls():
+
+    urls = parse_urls()
 
     assert(type(urls)) == List[str]
