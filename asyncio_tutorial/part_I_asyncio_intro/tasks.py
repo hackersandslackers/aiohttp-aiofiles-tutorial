b670c3b1c72a52b1efe9956a3af689c8813d2868
toddbirchard <toddbirchard@gmail.com>
2021-10-13 00:00:51 -0400

    Use correct type checking for Coroutines.

--- asyncio_tutorial/part_I_asyncio_intro/tasks.py
+++ asyncio_tutorial/part_I_asyncio_intro/tasks.py
@@ -1,10 +1,10 @@
 """Create task from coroutine which displays a value after a delay."""
 import asyncio
 from asyncio import Task
-from typing import Callable
+from typing import Coroutine
 
 
-async def create_task(coroutine: Callable) -> Task:
+async def create_task(coroutine: Coroutine) -> Task:
     """
     Create asyncio tasks to be executed.
 
